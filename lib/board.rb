2bb10d4cfed8ac0afe17c229b765ae2b206a58bf
DanielT94 <daniel.turnbull94@gmail.com>
2022-11-15 12:57:18 +0000

    COmpleted board class

--- lib/board.rb
+++ lib/board.rb
@@ -18,4 +18,54 @@ class Board
     end
   end
 
+  def update_board(row, column, symbol)
+    @grid[row][column] = symbol
+  end
+
+  def full_column?(input)
+    @grid[0][input] != empty_circle
+  end
+
+  def available_row(column)
+    row = 0
+    loop do
+      return row if row == 5 || @grid[row + 1][column] != empty_circle
+
+      row += 1
+    end
+  end
+
+  def check_vertical(row, column, symbol)
+    return if row > 2
+
+    @grid[row][column] == symbol && @grid[row + 1][column] == symbol && @grid[row + 2][column] == symbol && @grid[row + 3][column] == symbol
+  end
+
+  def check_horizontal(row, column, symbol)
+    return if column > 3
+
+    @grid[row][column] == symbol && @grid[row][column + 1] == symbol && @grid[row][column + 2] == symbol && @grid[row][column + 3] == symbol
+  end
+
+  def check_diagonals(row, column, symbol)
+    return unless row < 3
+
+    check_up_diagonal(row, column, symbol) || check_down_diagonal(row, column, symbol)
+  end
+
+  def check_down_diagonal(row, column, symbol)
+    return if column > 3
+
+    @grid[row][column] == symbol && @grid[row + 1][column + 1] == symbol && @grid[row + 2][column + 2] == symbol && @grid[row + 3][column + 3] == symbol
+  end
+
+  def check_up_diagonal(row, column, symbol)
+    return if column < 3
+
+    @grid[row][column] == symbol && @grid[row + 1][column - 1] == symbol && @grid[row + 2][column + 2] == symbol && @grid[row + 3][column + 3] == symbol
+  end
+
+  def check_four(row, column, symbol)
+    check_horizontal(row, column, symbol) || check_vertical(row, column, symbol) || check_diagonals(row, column, symbol)
+  end
 end
